f99c34e2ba65e572925d52e8bc61df62aeae4121
Reda Rahali <reda.rahali@ericsson.com>
2019-11-20 13:38:21 +0300

    'module1-submission_2'

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -11,8 +11,16 @@ function lunchCheckController($scope, $filter){
 
   $scope.message = "";
 
-  $scope.checkBtnClick = function(){
-
+  $scope.checkBtnClick = function()
+  {
+    $scope.message_color = 'green';
+    $scope.border_color = 'green';
+    if(!$scope.dishes || $scope.dishes.length === 0 ){
+      $scope.message_color = 'red';
+      $scope.border_color = 'red';
+      $scope.message = 'Please enter data first';
+      return;
+    }
     if (numberOfWords($scope.dishes) > 3 ){
       $scope.message = 'Too much!';
     }else{
@@ -26,7 +34,7 @@ function lunchCheckController($scope, $filter){
 function numberOfWords(string){
   var number = 0;
 
-  var words = string.split(' ');
+  var words = string.split(",");
   for (var i = 0; i < words.length; i++){
     console.log(words[i]);
   }
